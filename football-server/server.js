8c916f3fe08a6adb8fff599e74140129e84007a6
ArchanaManju <archanahm2021@gmail.com>
2026-01-29 12:41:51 +0000

    servere port

--- football-server/server.js
+++ football-server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const xlsx = require('xlsx')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const cors = require('cors');
 const fs = require('fs');
 
